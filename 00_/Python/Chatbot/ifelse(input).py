c830f774370c4d5c469abd7b9c36b35fd5231442
Ian <passionsoft.ian@gmail.com>
2023-01-12 17:23:37 +0900

    2일차 python 문법|230112

---
+++ 00_/Python/Chatbot/ifelse(input).py
@@ -0,0 +1,6 @@
+dust = int(input())
+if dust > 50:
+    print('50 초과')
+
+else:
+    print('50이하')
---
+++ 00_/Python/Chatbot/ifelse.py
@@ -0,0 +1,9 @@
+dust = 31
+if dust < 50:
+    print('50 미만')
+elif dust > 50:
+    print('50 초과')
+elif dust > 30:
+    print('30 초과')
+else:
+    print('30 이하')
